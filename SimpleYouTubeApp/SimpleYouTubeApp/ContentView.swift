664ac9a5ad3f406ca98873142287a7cba82dec03
Ronnie Vega <9502820+ronnyvega@users.noreply.github.com>
2022-12-12 15:02:17 -0800

    Add basic dependencies

--- SimpleYouTubeApp/SimpleYouTubeApp/ContentView.swift
+++ SimpleYouTubeApp/SimpleYouTubeApp/ContentView.swift
@@ -7,15 +7,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationSplitView {
+            /*@START_MENU_TOKEN@*/Text("Sidebar")/*@END_MENU_TOKEN@*/
+        } detail: {
+            NavigationStack {
+                ScrollView {
+                    Grid {
+                        ForEach(0 ..< 3) { _ in
+                            GridRow {
+                                ForEach(0 ..< 3) { _ in
+                                    GridCell()
+                                }
+                            }
+                        }
+                        .padding()
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Home")
         }
-        .padding()
     }
 }
+
+private struct GridCell: View {
+    var body: some View {
+        Color
+            .primary
+            .frame(width: 100, height: 80)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
