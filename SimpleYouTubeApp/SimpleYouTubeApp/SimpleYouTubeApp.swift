3fb69f94f39338cff78c26276a952c812994ccb5
Ronnie Vega <9502820+ronnyvega@users.noreply.github.com>
2022-12-11 18:53:22 -0800

    Add Xcode project

---
+++ SimpleYouTubeApp/SimpleYouTubeApp/SimpleYouTubeApp.swift
@@ -0,0 +1,15 @@
+//
+//  SimpleYouTubeApp.swift
+//  SimpleYouTubeApp
+//
+
+import SwiftUI
+
+@main
+struct SimpleYouTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
